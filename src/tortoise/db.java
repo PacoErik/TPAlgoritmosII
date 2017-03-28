3b4062252b2f3a23bbd4cb9f3fd86078c29fba26
Erik Paco <erik@paco.16mb.com>
2017-03-28 11:09:01 -0300

    Borrados los comentarios sin utilidad

--- src/tortoise/db.java
+++ src/tortoise/db.java
@@ -2,20 +2,12 @@ package tortoise;
 
 import java.sql.DriverManager;
 import java.lang.annotation.Annotation;
-import java.lang.reflect.AnnotatedElement;
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.SQLIntegrityConstraintViolationException;
 import java.sql.Statement;
 import java.util.List;
-
-import sun.reflect.annotation.AnnotationParser;
-
-import java.lang.reflect.Method;
 
 public class db {
 	public static String getAnnotationValue(Annotation a){
@@ -38,16 +30,6 @@ public class db {
 		return a;
 	}
 	public static int insert(Connection con, Object obj) throws IllegalArgumentException, IllegalAccessException, SQLException{
-		/*for (Annotation a:obj.getClass().getDeclaredAnnotations()){
-			System.out.printf("%n%s",getAnnotationValue(a));
-		}
-		for (Field f:obj.getClass().getDeclaredFields()){
-			for (Annotation a:f.getAnnotations()){
-				System.out.printf("%n%s %s",f.getName(),getAnnotationValue(a));
-			}
-			System.out.printf("%n%s", f.get(obj));
-			
-		}*/
 		Statement st = con.createStatement();
 		String tql = "INSERT INTO ";
 		tql+=obj.getClass().getAnnotation(Table.class).name()+"(";
@@ -84,8 +66,6 @@ public class db {
 	}
 	
 	public static void main(String[] args) throws SQLException, ClassNotFoundException, IllegalArgumentException, IllegalAccessException{
-		PreparedStatement pstm = null;
-		ResultSet rs = null;
 		Connection c = null;
 		
 		Class.forName("org.hsqldb.jdbcDriver");
@@ -104,33 +84,5 @@ public class db {
 		dep.location = "Hola";
 		int r = tortoise.db.insert(c,dep);		
 		System.out.println(r);
-		/*
-		Statement stm = c.createStatement();
-		
-		rs = stm.executeQuery("SELECT EMP_NAME FROM EMPLOYEE WHERE JOB='SALESMAN'");
-		while (rs.next()) {
-			System.out.println("EMPLOYEE Name:"
-					+ rs.getString("EMP_NAME"));
-		}
-		*/
-		
-		/*
-		String sql ="";
-		sql+="CREATE TABLE Personas (";
-		sql+="IdPersona int,";
-		sql+="Apellidos varchar(255),";
-		sql+="Nombres varchar(255),";
-	    sql+="Direccion varchar(255),";
-		sql+="Ciudad varchar(255)";
-		sql+=");";
-	
-		pstm = c.prepareStatement(sql);
-		int rtdo = pstm.executeUpdate();
-		if (rtdo == 1){
-			System.out.println("Tabla insertada");
-		}
-		else {
-			System.out.println("Fallo al insertar tabla");
-		}*/
 	}
 }
