299fb93d3b046d18398d0e71220a0839ebb85763
Erik Paco <erik@paco.16mb.com>
2017-03-28 10:59:21 -0300

    Con la implementación actual, no se necesitan getters/setters

--- src/tortoise/Department.java
+++ src/tortoise/Department.java
@@ -12,30 +12,6 @@ public class Department {
 	@Column(name="LOCATION")
 	String location;
 	
-	public int getDeptId() {
-		return deptId;
-	}
-	public void setDeptId(int deptId) {
-		this.deptId = deptId;
-	}
-	public String getDeptName() {
-		return deptName;
-	}
-	public void setDeptName(String deptName) {
-		this.deptName = deptName;
-	}
-	public String getDeptNo() {
-		return deptNo;
-	}
-	public void setDeptNo(String deptNo) {
-		this.deptNo = deptNo;
-	}
-	public String getLocation() {
-		return location;
-	}
-	public void setLocation(String location) {
-		this.location = location;
-	}
 
 	
 }
